6caf165cd0249427c0270d829639f239722ac185
wh1te909 <dcparsi@gmail.com>
2019-10-21 20:44:50 -0700

    add more tests

--- tests/test_agent.py
+++ tests/test_agent.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import validators
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..')))
 
@@ -26,3 +27,28 @@ def test_services():
     assert type(services) is list
     assert spooler["display_name"] == "Print Spooler"
     assert spooler["username"] == "LocalSystem"
+
+def test_disks():
+    disks = winutils.get_disks()
+    assert disks["C:"]["device"] == "C:"
+
+def test_cpu_info():
+    assert type(winutils.get_cpu_info()[0]["physical_cores"]) is int
+
+def test_os():
+    assert "Microsoft Windows" in winutils.get_os()
+
+def test_cmd_output():
+    output = winutils.get_cmd_output("ping 8.8.8.8")
+    success = ["Reply", "bytes", "time", "TTL"]
+    assert all(x in output for x in success)
+
+def test_public_ip():
+    error = []
+    if not validators.ipv4(winutils.get_public_ip()) and not validators.ipv6(winutils.get_public_ip()):
+        error.append("not ipv4 or ipv6")
+    
+    assert not error
+
+def test_platform_release():
+    assert 'windows' in winutils.get_platform_release().lower()
